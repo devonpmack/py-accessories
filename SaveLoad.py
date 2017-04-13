6bf18eb6b029278197074696f7c1cca322336172
Devon Mack <devonpmack@gmail.com>
2017-04-13 14:43:49 -0400

    Initial commit

---
+++ SaveLoad.py
@@ -0,0 +1,23 @@
+# Written by Devon Mack April 2017
+# This class can be used to easily store variables to a file and load them back later
+
+import json
+
+
+class SaveLoad(object):
+    def __init__(self, to_load=str()):
+        if len(to_load) > 0:
+            with open(to_load) as f:
+                self.__dict__ = json.load(f)
+
+    def dump(self, file_name):
+        f = open(file_name, "w")
+        json.dump(self.__dict__, f, sort_keys=True, indent=4, separators=(',', ': '))
+
+    def load(self, file_name):
+        try:
+            f = open(file_name, "r")
+            self.__dict__ = json.load(f)
+        except FileNotFoundError:
+            print("File " + file_name + " not found!")
+
