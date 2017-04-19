b00c2fdb59a7ecfcefa56ba053a673eef4074689
devonpmack <devonpmack@gmail.com>
2017-04-19 14:01:57 -0400

    can now specify whether you would like to create the file if it doesn't exist

--- SaveLoad.py
+++ SaveLoad.py
@@ -5,21 +5,24 @@ import json
 
 
 class SaveLoad(object):
-    def __init__(self, file_name=str()):
-        try:
-            if len(file_name) > 0:
-                with open(file_name) as f:
-                    self.__dict__ = json.load(f)
-        except FileNotFoundError:
-            print("[SaveLoad Error] Can't find file " + file_name + "!")
+    def __init__(self, file_name="", create=False):
+        if file_name != "":
+            self.load(file_name, create)
 
     def dump(self, file_name):
         f = open(file_name, "w")
         json.dump(self.__dict__, f, sort_keys=True, indent=4, separators=(',', ': '))
+        f.close()
 
-    def load(self, file_name):
+    def load(self, file_name, create=False):
         try:
             f = open(file_name, "r")
             self.__dict__ = json.load(f)
+            f.close()
         except FileNotFoundError:
-            print("[SaveLoad Error] Can't find file " + file_name + "!")
+            if create:
+                print("[SaveLoad] Creating file " + file_name)
+                f = open(file_name, "w")
+                f.close()
+            else:
+                raise FileNotFoundError
