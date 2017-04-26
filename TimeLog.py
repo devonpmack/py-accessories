4310714e53dab7d2ce225acce29d16a2e69c96b0
devonpmack <devonpmack@gmail.com>
2017-04-26 13:19:52 -0400

    Can choose colour for text now (number between 30-37)

--- TimeLog.py
+++ TimeLog.py
@@ -1,15 +1,26 @@
 import datetime as dt
-
+import re
 
 class Timer(object):
     def __init__(self):
         self.start_time = dt.datetime.now()
+        self.colour = None
 
     def time_str(self):
-        return "[Elapsed time: %.2f] " % (dt.datetime.now() - self.start_time).total_seconds()
+        if type(self.colour) is not str:
+            return "[Elapsed time: %.2f] " % (dt.datetime.now() - self.start_time).total_seconds()
+        else:
+            return self.colour + "[Elapsed time: %.2f] " % (dt.datetime.now() - self.start_time).total_seconds() + \
+                   self.colour
 
     def reset(self):
-        self.start_time = dt.date.now()
+        self.start_time = dt.datetime.now()
 
     def time_print(self, to_print):
         print(self.time_str() + to_print)
+
+    def set_colour(self, colour):
+        if 30 <= colour <= 37:
+            self.colour = "\\033[1;" + str(colour) + ";40m"
+        else:
+            print("[Warning] Invalid colour!")
